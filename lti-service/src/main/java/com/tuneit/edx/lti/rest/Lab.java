24fd7966fd68cae9b1712ea913ce78246fb61bad
Alexey Pismak <alex@tune-it.ru>
2018-11-26 13:29:42 +0300

    Add trace for http request headers and body

--- lti-service/src/main/java/com/tuneit/edx/lti/rest/Lab.java
+++ lti-service/src/main/java/com/tuneit/edx/lti/rest/Lab.java
@@ -1,17 +1,29 @@
 package com.tuneit.edx.lti.rest;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import org.imsglobal.aspect.Lti;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Lab {
 
     @Lti
     @PostMapping("/api/rest/lti/{labId}")
-    public String doPost(@PathVariable("labId") String labId) {
+    public String doPost(@PathVariable("labId") String labId, @RequestHeader HttpHeaders headers, @RequestBody String body) {
+
+        System.out.println("####################################### REQUEST BODY");
+        System.out.println(body);
+        System.out.println("####################################### REQUEST HEADERS");
+
+        for(String key : headers.keySet()) {
+            if(headers.getValuesAsList(key).isEmpty()) {
+                continue;
+            }
+            headers.getValuesAsList(key).forEach(value ->
+                System.out.println(key + " ===> " + value)
+            );
+        }
+
         return "Task for lab work #" + labId;
     }
 
