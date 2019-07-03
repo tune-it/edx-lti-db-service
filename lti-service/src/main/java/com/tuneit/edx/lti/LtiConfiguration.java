5c533a323f1de64856d14c2cb0121c799a6aac2f
Alexandr Isaev <isaevalex1199@gmail.com>
2019-07-03 14:25:13 +0300

    fix: add spring configuration

---
+++ lti-service/src/main/java/com/tuneit/edx/lti/LtiConfiguration.java
@@ -0,0 +1,9 @@
+package com.tuneit.edx.lti;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.tuneit.edx.lti")
+public class LtiConfiguration {
+}
